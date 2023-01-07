56419f7c7ab51c8a2d70924638ab9a2c0dcac73a
Tejas <rao.tejas01@gmail.com>
2023-01-07 21:07:08 +0530

    initial commit

---
+++ Classroom/sweeper_class.py
@@ -0,0 +1,11 @@
+import os
+import bpy
+import numpy as np
+beg_val = 1.0
+end_val = 3.0
+x = np.linspace(beg_val, end_val, 41)
+output_dir = r"C:\Users\CFI Workstation\Documents\Blender\BarcaArchVis\Multifocal""\\"
+for i in x:
+    bpy.context.object.data.dof.focus_distance = i
+    bpy.context.scene.render.filepath = os.path.join(output_dir, "vis_%.2f.png" % i)
+    bpy.ops.render.render(write_still = True)
